407ec8154bd9c8e4b0cc83a971fbe16eaf4b698f
Pepigis <cernyjosef093@gmail.com>
2025-01-14 08:49:53 +0100

    nefunguje

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 module.exports = function(eleventyConfig) {
-    // Kopírování CSS a obrázků do výstupní složky
-    eleventyConfig.addPassthroughCopy("src/css");
-    eleventyConfig.addPassthroughCopy("src/images");
-    return {
-      dir: {
-        input: "src",
-        output: "dist", // Výstupní složka
-      }
-    };
-   };
+  // Kopírovat statické soubory (CSS, obrázky)
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "dist"
+    }
+  };
+ };
